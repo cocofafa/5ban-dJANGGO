01e5cb32ebe1c540b9902e4a2a789d60061383f4
cocofafa <dlduddn528@naver.com>
2021-07-26 10:40:07 +0900

    07.26 class commit_3

--- profileapp/models.py
+++ profileapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 
-class profile(models.Model):
+class Profile(models.Model):
     user =  models.OneToOneField(User, on_delete=models.CASCADE,
                                  related_name= 'profile')
 
